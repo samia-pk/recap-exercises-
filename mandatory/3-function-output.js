a2504f9deadfb8d1855c26ed799df12774f65e45
samia-pk <73727003+samia-pk@users.noreply.github.com>
2021-04-22 13:51:16 +0100

    Initial commit

---
+++ mandatory/3-function-output.js
@@ -0,0 +1,55 @@
+// Add comments to explain what this function does. You're meant to use Google!
+function getRandomNumber() {
+  return Math.random() * 10;
+}
+
+// Add comments to explain what this function does. You're meant to use Google!
+function combine2Words(word1, word2) {
+  return word1.concat(word2);
+}
+
+function concatenate(firstWord, secondWord, thirdWord) {
+  // Write the body of this function to concatenate three words together.
+  // Look at the test case below to understand what this function is expected to return.
+}
+
+/* 
+===================================================
+======= TESTS - DO NOT MODIFY BELOW THIS LINE =====
+
+There are some Tests in this file that will help you work out if your code is working.
+
+To run these tests type `node 3-function-output` into your terminal
+==================================
+*/
+
+const util = require("util");
+
+function test(test_name, actual, expected) {
+  let status;
+  if (actual === expected) {
+    status = "PASSED";
+  } else {
+    status = `FAILED: expected: ${util.inspect(
+      expected
+    )} but your function returned: ${util.inspect(actual)}`;
+  }
+
+  console.log(`${test_name}: ${status}`);
+}
+
+test(
+  "concatenate function - case 1 works",
+  concatenate("code", "your", "future"),
+  "code your future"
+);
+test(
+  "concatenate function - case 2 works",
+  concatenate("I", "like", "pizza"),
+  "I like pizza"
+);
+test(
+  "concatenate function - case 3 works",
+  concatenate("I", "am", 13),
+  "I am 13"
+);
